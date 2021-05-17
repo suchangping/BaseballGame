a21dad8a875084419d1d255ce4d1ce28636b77b3
10072002su_changping@cn.tre-inc.com <10072002su_changping@cn.tre-inc.com>
2021-05-17 10:18:15 +0900

    Initial commit

---
+++ src/game/main.java
@@ -0,0 +1,19 @@
+package game;
+
+public class main {
+    public static void main(String[] args) {
+        Team teamA = new Team();
+        Team teamB = new Team();
+
+        teamA.setName("ソフトバンク");
+        teamB.setName("");
+
+        for (int i = 0; i < 9; i++){
+            if(i%2 == 1) {
+
+            }else {
+
+            }
+        }
+    }
+}
